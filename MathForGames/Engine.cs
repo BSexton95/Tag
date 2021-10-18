845ecc8c92408dd115e6f5c171f0d4392b093a78
BSexton95 <88592703+BSexton95@users.noreply.github.com>
2021-10-18 15:21:56 -0500

    Initialize Raylib

---
+++ MathForGames/Engine.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        private static bool _applicationShouldClose;
+        private static int _currentSceneIndex;
+        private Scene[] _scenes = new Scene[0];
+        private Stopwatch _stopWatch = new Stopwatch();
+
+        /// <summary>
+        /// Called to begin the application
+        /// </summary>
+        public void Run()
+        {
+            //Call start for the entire application
+            Start();
+
+            float currentTime = 0;
+            float lastTime = 0;
+            float deltaTime = 0;
+
+            //Loop until the application is told to close
+            while (!_applicationShouldClose && !Raylib.WindowShouldClose())
+            {
+                //Get how much time has passed since the application started
+                currentTime = _stopWatch.ElapsedMilliseconds / 1000.0f;
+
+                //Set delta time to be the difference in time from the last time recorded to the current time
+                deltaTime = currentTime - lastTime;
+
+                //Update the application
+                Update(deltaTime);
+                //Draw all items
+                Draw();
+
+                //Set the last time recorded to be the current time
+                lastTime = currentTime;
+            }
+
+            //Call end for the entire application
+            End();
+        }
+
+        /// <summary>
+        /// Called when the application starts
+        /// </summary>
+        private void Start()
+        {
+            _stopWatch.Start();
+
+            //Create a window using raylib
+            Raylib.InitWindow(800, 450, "Math for Games");
+            Raylib.SetTargetFPS(0);
+
+            Scene scene = new Scene();
+
+            Player player = new Player('@', 10, 10, 100, Color.VIOLET, "Player");
+            Actor enemy = new Actor('A', 200, 5, Color.RED, "Enemy");
+
+            scene.AddActor(enemy);
+            scene.AddActor(player);
+
+            _currentSceneIndex = AddScene(scene);
+            _scenes[_currentSceneIndex].Start();
+
+            Console.CursorVisible = false;
+        }
+
+        /// <summary>
+        /// Called evertime the game loops
+        /// </summary>
+        private void Update(float deltaTime)
+        {
+            _scenes[_currentSceneIndex].Update(deltaTime);
+        }
+
+        /// <summary>
+        /// Called every time the game loops to update visuals
+        /// </summary>
+        private void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            //Adds all actor icons to buffer
+            _scenes[_currentSceneIndex].Draw();
+
+            Raylib.EndDrawing();
+        }
+
+        /// <summary>
+        /// Called when the apllication exits
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+            Raylib.CloseWindow();
+
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Adds a scene to the engine's scene array
+        /// </summary>
+        /// <param name="scene">The scene that will be added to the scene array</param>
+        /// <returns>The index that the new scene is located</returns>
+        public int AddScene(Scene scene)
+        {
+            //Create a new temporary array
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            //Copy all values from old array into the new array
+            for (int i = 0; i < _scenes.Length; i ++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+
+            //Set the last index to be the new scene
+            tempArray[_scenes.Length] = scene;
+
+            //Set the old array to be the new array
+            _scenes = tempArray;
+
+            //Return the last index
+            return _scenes.Length - 1;
+        }
+
+        /// <summary>
+        /// Gets the next key in the input stream 
+        /// </summary>
+        /// <returns>The key that was pressed</returns>
+        public static ConsoleKey GetNextKey()
+        {
+            //If there is no key being pressed...
+            if (!Console.KeyAvailable)
+                //...return
+                return 0;
+
+            //Return the current key being pressed
+            return Console.ReadKey(true).Key;
+        }
+
+        /// <summary>
+        /// Ends the application
+        /// </summary>
+        public static void CloseApplication()
+        {
+            _applicationShouldClose = true;
+        }
+    }
+}
---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public struct Vector2
+    {
+        public float X;
+        public float Y;
+
+        public Vector2 (float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Gets the length of the vector
+        /// </summary>
+        public float Magnitude
+        {
+            get 
+            { 
+                return (float)Math.Sqrt(X * X + Y * Y); 
+            }
+        }
+
+        /// <summary>
+        /// Gets the normalized version of this vector without changing it
+        /// </summary>
+        public Vector2 Normalized
+        {
+            get
+            {
+                Vector2 value = this;
+                return value.Normalize();
+            }
+        }
+
+        /// <summary>
+        /// Changes this vector to have a magnitude that is equal to one
+        /// </summary>
+        /// <returns>The result of the normalization. Returns an emply vector if the magnitude is zero</returns>
+        public Vector2 Normalize()
+        {
+            if (Magnitude == 0)
+                return new Vector2();
+
+            return this / Magnitude;
+        }
+
+        /// <summary>
+        /// Adds the x value of the second vector to the first, and adds the y value of the second vector to the first.
+        /// </summary>
+        /// <param name="lhs">The vector that is increasing</param>
+        /// <param name="rhs">The vector used to increase the 1st vectre</param>
+        /// <returns>The reult of the vector addition</returns>
+        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2 { X = lhs.X + rhs.X, Y = lhs.Y + rhs.Y };
+        }
+
+        ///<summary>
+        /// Subtracts the x value of the second vector to the first, and Sibtracts the y value of the second vector to the first.
+        /// </summary>
+        /// <param name="lhs">The vector that is being subtracted from</param>
+        /// <param name="rhs">The vector used to subtract from the first</param>
+        /// <returns>The reult of the vector subtraction</returns>
+        public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2 { X = lhs.X + rhs.X, Y = lhs.Y + rhs.Y };
+        }
+
+        /// <summary>
+        /// Multiplies the vector's x and y values by the scalar
+        /// </summary>
+        /// <param name="vector">The vector that is being scaled</param>
+        /// <param name="scalar">The value to scale the vector by</param>
+        /// <returns>The result of the vector scaling</returns>
+        public static Vector2 operator *(Vector2 vector, float scalar)
+        {
+            return new Vector2 { X = vector.X * scalar, Y = vector.Y * scalar };
+        }
+
+        /// <summary>
+        /// Divides the vector's x and y values by the scalar given
+        /// </summary>
+        /// <param name="vector">The vector that is being scaled</param>
+        /// <param name="scalar">The value to scale the vector by</param>
+        /// <returns>The result of the vector scaling</returns>
+        public static Vector2 operator /(Vector2 vector, float scalar)
+        {
+            return new Vector2 { X = vector.X / scalar, Y = vector.Y / scalar };
+        }
+
+        /// <summary>
+        /// Compares the x and y values of two vectors
+        /// </summary>
+        /// <param name="lhs">The left side of the comparison</param>
+        /// <param name="rhs">The right side of the comparison</param>
+        /// <returns>True if the x values of both vectors match and the y values match</returns>
+        public static bool operator ==(Vector2 lhs, Vector2 rhs) 
+        {
+            return lhs.X == rhs.X && lhs.Y == rhs.Y;
+        }
+
+        /// <summary>
+        /// Compares the x and y values of two vectors
+        /// </summary>
+        /// <param name="lhs">The left side of the comparison</param>
+        /// <param name="rhs">The right side of the comparison</param>
+        /// <returns>True if the x values of both vectors don't match and the y values don't match</returns>
+        public static bool operator !=(Vector2 lhs, Vector2 rhs)
+        {
+            return lhs.X != rhs.X || lhs.Y != rhs.Y;
+        }
+
+    }
+}
